795325e83503bd93c717374e580473ed0de9c90d
polarbean <harry.carey95@gmail.com>
2025-04-30 16:36:58 +0200

    fix sorting error

--- add_animal_numbers.py
+++ add_animal_numbers.py
@@ -20,17 +20,31 @@ for k, v in output.items():
         None
 output.keys()
 
-
 def sort_dict(x):
+    # Get the values for all keys
     gene_name = x["gene_name"]
+    gene_description = x["gene_description"]
+    synonyms = x["synonyms"]
+    ensembl_ids = x["ensembl_ids"]
     number_of_animals = x["number_of_animals"]
-    combined_list = list(zip(gene_name, number_of_animals))
-    sorted_list = sorted(combined_list, key=lambda x: x[1], reverse=True)
-    gene_name, number_of_animals = zip(*sorted_list)
-    x["gene_name"] = list(gene_name)
-    x["number_of_animals"] = list(number_of_animals)
-    return x
 
+    # Create a list of tuples with all values at each position
+    combined_list = list(zip(gene_name, gene_description, synonyms, ensembl_ids, number_of_animals))
+
+    # Sort based on number_of_animals (index 4)
+    sorted_list = sorted(combined_list, key=lambda x: x[4], reverse=True)
+
+    # Unpack the sorted values
+    gene_name, gene_description, synonyms, ensembl_ids, number_of_animals = zip(*sorted_list)
+
+    # Update the dictionary with sorted values
+    x["gene_name"] = list(gene_name)
+    x["gene_description"] = list(gene_description)
+    x["synonyms"] = list(synonyms)
+    x["ensembl_ids"] = list(ensembl_ids)
+    x["number_of_animals"] = list(number_of_animals)
+
+    return x
 
 output = sort_dict(output)
 with gzip.open("data/gene_data_counts.json.gz", "wt") as f:
